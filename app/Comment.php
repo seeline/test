56aac842b180a16d0e60dc58ecbecb70e24e5291
Unknown <ch.cline09@gmail.com>
2017-06-12 14:50:50 -0700

    6/12/17
    Laravel 15 - allowing comments to be shown

--- app/Comment.php
+++ app/Comment.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
 class Comment extends Model
 {
-    //
+    //$comment->post
+    public function post()
+    {
+      return $this->belongsTo(Post::class);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,31 @@
 		<h1>{{ $post->title}}</h1>
 
 		{{$post->body}}
-		
+
+		<hr>
+
+		<div class="comments">
+
+			<ul class="list-group">
+
+			@foreach ($post->comments as $comment)
+
+				<li class="list-group-item">
+
+					<strong>
+
+					{{$comment->created_at->diffForHumans() }}: &nbsp;
+
+					</strong>
+
+					{{$comment->body}}
+
+			@endforeach
+
+		</ul>
+
+		</div>
+
 	</div>
+
 @endsection
